ad9e34cf69bb992de289c420142fd1903b0ef326
jdacosta1991 <workdacojo@gmail.com>
2022-03-26 16:33:08 -0400

    se agrego seguridad en las credenciales de la API correctamente

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+#----DEFINIMOS EL METODO PARA OBTENER EL TICKER DESDE LA API
+    def self.new_lookup(ticker_symbol)
+#----CODIGO PROPORCIONADO DESDE LA DOC DE LA API
+			client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:cloud_api_key],
+                              endpoint: 'https://cloud.iexapis.com/v1')
+			client.quote(ticker_symbol).latest_price
+    end
 end
