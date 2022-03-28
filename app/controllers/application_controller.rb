38d90f5ff6bc0658edb46da3457718d09c551274
jdacosta1991 <workdacojo@gmail.com>
2022-03-28 11:45:26 -0400

    Add edit user and others parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
 #------ANTES DE CUALQUIER ACCION AUNTENTICA USUARIO----- 	
 	before_action :authenticate_user!
+#------ ADD ADITIONAL FIELDS PARA EDITAR USUARIO  
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+  		devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+	end
+	
+
+	
+	
 end
