40b81f8f3079e11164d9a60100a6dba797b7e17b
jdacosta1991 <workdacojo@gmail.com>
2022-03-27 23:06:51 -0400

    Add some obj

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,8 +6,18 @@ class UserStocksController < ApplicationController
       stock = Stock.new_lookup(params[:ticker])
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
-    flash[:notice] = "The Stock #{stock.ticker} #{stock.name} is add to your portfolio"
+    flash[:notice] = "The Stock #{stock.ticker}, #{stock.name} is add to your portfolio"
     redirect_to my_portfolio_path
   end
+  #----accion para eliminar stocks de usuarios
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id,
+                                  stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} is successfully removed"
+    redirect_to my_portfolio_path
+  end
+  
   
 end
