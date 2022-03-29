bf57a83e727ca0bcf500c310d696d2fae23adb59
jdacosta1991 <workdacojo@gmail.com>
2022-03-29 00:22:24 -0400

    add friends controller models and funcionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,32 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+  def my_friends
+    @friends = current_user.friends
+  end
+  def search
+    #--CONDICIONAL PARA BUSQUEDAS INVALIDAS
+     if params[:friend].present? 
+       @friend = params[:friend]
+       #--LINEA PARA QUE JAVASCRIPT MANEJE EL RESULTADO Y LAS ALERTAS
+       if @friend
+         respond_to do |format|
+           format.js { render partial: 'users/friend_result' }
+         end
+       else
+         respond_to do |format|
+           #--LOS MENSAJES FLASH.NOW PARA QUE NO TENGAN PERSISTENCIA EN LA PAG
+           flash.now[:alert] = "Couldn't find user"
+           format.js { render partial: 'users/friend_result' }
+         end
+       end
+     else 
+       respond_to do |format|
+         flash.now[:alert] = "Please enter a friend name or email to search"
+         format.js { render partial: 'users/friend_result' }
+       end
+     end
+   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
 #---- RUTA PARA MOSTRAR EL VALOR DE STOCK UNA VEZ QUE SE HAGA LA BUSQUEDAD
   get 'search_stock', to: 'stocks#search'
-
+#---- Ruta para amigos
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
 end
